5658924f2f9f6a58cd10751f8299a54241270f7e
rcaws22 <49497149+rcaws22@users.noreply.github.com>
2026-01-30 10:18:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ provider "google"{
 # Calling vpc module
 module "vpc" {
     # the below is for https connection using Personall access token 
-    source = "git::https://github.com/i27devopsb7/terraform-gcp-modules.git//vpc?ref=v1.0.0"
+    source = "git::https://github.com/rcaws22/terraform-gcp-modules.git//vpc?ref=v1.0.0"
 
     # the below is for ssh connection, if we are using keys ~/.ssh/id_rsa
-    #source = "git@github.com:i27devopsb7/terraform-gcp-modules.git//vpc?ref=v1.0.0"
+    #source = "git@github.com:rcaws22/terraform-gcp-modules.git//vpc?ref=v1.0.0"
     vpc_name = var.vpc_name
 }
 
 
 # Calling Subnet Module
 module "subnet" {
-  source = "git::https://github.com/i27devopsb7/terraform-gcp-modules.git//subnet?ref=v1.0.0"
+  source = "git::https://github.com/rcaws22/terraform-gcp-modules.git//subnet?ref=v1.0.0"
   subnet_name = var.subnet_name
     region = var.region
     vpc_id = module.vpc.vpc_id
@@ -27,7 +27,7 @@ module "subnet" {
 
 # Calling GCE Module
 module "gce" {
-  source = "git::https://github.com/i27devopsb7/terraform-gcp-modules.git//gce?ref=v1.0.0"
+  source = "git::https://github.com/rcaws22/terraform-gcp-modules.git//gce?ref=v1.0.0"
   vm_name = var.vm_name
   zone = var.zone
   machine_type = var.machine_type
